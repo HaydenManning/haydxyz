e96fe493be0ee714827ca4092f2a1b9ab345fd3e
Hayden Manning <haydenmanning@aol.com>
2018-02-26 14:46:41 -0600

    creating table

--- src/components/UrlShortener/NewShortUrl/NewShortUrl.js
+++ src/components/UrlShortener/NewShortUrl/NewShortUrl.js
@@ -28,6 +28,8 @@ class NewShortUrl extends Component {
               <input
                 id="long-input"
                 onChange={e => this.newShortUrlInput(e.target.value)}
+                placeholder="Your original URL here"
+                type="url"
               />
               <button onClick={() => this.createNewShortUrl()}>
                 SHORTEN URL
@@ -39,6 +41,21 @@ class NewShortUrl extends Component {
             </p>
           </div>
         </div>
+        <div className="url-shortener-list">
+          <div className="table-head">
+            <h2 id="table-head-original">Original URL</h2>
+            <h2 id="table-head-created">Created</h2>
+            <h2 id="table-head-short">Short URL</h2>
+            <h2 id="table-head-click">All Clicks</h2>
+          </div>
+          <h1>
+            This will be a list of the last 10 generated url's (either for the
+            user of for the entire site) (could also do top visited links
+            instead)
+          </h1>
+        </div>
+
+        {/* conditional rendering for the screen prompt that provides users with the new short link*/}
       </div>
     );
   }
