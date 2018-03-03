4acf0c24c57bdfae91109e3235c92b41ea86bc6d
Hayden Manning <haydenmanning@aol.com>
2018-03-03 05:46:42 -0600

    removed user section

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,6 @@ import { Switch, Route } from "react-router-dom";
 
 import Landing from "./components/Landing/Landing";
 import UrlRedirect from "./components/UrlShortener/UrlRedirect/UrlRedirect";
-import UserProfile from "./components/User/UserProfile/UserProfile";
-import UserSettings from "./components/User/UserSettings/UserSettings";
 import Donate from "./components/Donate/Donate";
 
 // short url regex
@@ -13,14 +11,12 @@ import Donate from "./components/Donate/Donate";
 export default (
   <Switch>
     <Route exact path="/" component={Landing} />
-    <Route path="/give" component={Donate} />
-    <Route path="/user/:id/settings" component={UserSettings} />
-    <Route path="/user/:id" component={UserProfile} />
+    <Route path="/i/give" component={Donate} />
     <Route // THIS ROUTE MUST BE ON BOTTOM
       sensitive
       path="/:shortURL"
       render={({ match }) => {
-        if (!/^[a-zA-Z0-9]{5}$/.test(match.params.shortURL)) {
+        if (!/^[a-zA-Z0-9]{3-8}$/.test(match.params.shortURL)) {
           return <h1>404 Error</h1>;
         }
         return <UrlRedirect />;
