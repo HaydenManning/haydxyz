d3a9e407b60137d10d44d2b112215defe0b61baa
Hayden Manning <haydenmanning@aol.com>
2018-03-13 15:07:04 -0500

    header/footer content complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,41 +1,24 @@
 // Site Footer
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { withRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./Footer.css";
-import { getUser } from "./../../ducks/reducer";
 
 class Footer extends Component {
-  componentDidMount() {
-    this.props.getUser();
-  }
-
   render() {
     return (
       <div className="footer-main">
-        <div className="footer-link">
-          <Link to="/c/contact">
-            <h1>CONTACT</h1>
-          </Link>
-          {this.props.user.length > 0 ? (
-            <a href={process.env.REACT_APP_LOGIN}>
-              <h1>ADMIN</h1>
-            </a>
-          ) : (
-            <Link to="/a/dash">
-              <h1>ADMIN</h1>
-            </Link>
-          )}
-          <Link to="/give">
-            <h1>DONATE</h1>
-          </Link>
-        </div>
+        <p>
+          Made with love by{" "}
+          <a href="https://www.linkedin.com/in/thaydenmanning/">
+            Hayden Manning
+          </a>{" "}
+          | <a href="https://github.com/HaydenManning">GitHub</a> |{" "}
+          <Link to="/i/tos">Terms of Service</Link>
+        </p>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => state;
-
-export default withRouter(connect(mapStateToProps, { getUser })(Footer));
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,25 @@ class Header extends Component {
   render() {
     return (
       <div className="header-main">
-        <div className="header-logo">
+        <div className="header-left">
           <Link to="/">
-            <h1>HDN</h1>
+            <h1>HDN.MX</h1>
           </Link>
+          <a href="https://github.com/HaydenManning/hdnmx-url-shortener">
+            <h1>GitHub</h1>
+          </a>
         </div>
+        {/* auth_status needs to be created */}
+        {this.props.auth_status !== true ? (
+          <div className="header-right">
+            <button>Login / Signup</button>
+          </div>
+        ) : (
+          <div className="header-right">
+            <button>Logout</button>
+            <button>Settings</button>
+          </div>
+        )}
       </div>
     );
   }
