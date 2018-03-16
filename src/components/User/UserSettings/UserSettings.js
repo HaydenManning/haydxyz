14ca6c066766a510d64575bdb02192c4debaff9b
Hayden Manning <haydenmanning@aol.com>
2018-03-16 12:05:13 -0500

    settings content

--- src/components/User/UserSettings/UserSettings.js
+++ src/components/User/UserSettings/UserSettings.js
@@ -15,6 +15,47 @@ class USettings extends Component {
               ? `${this.props.user.f_name} ${this.props.user.l_name}`
               : "feel free to input some information."}
           </h1>
+          <div className="uinfo-three">
+            <div className="user-info">
+              <h2>Update First Name</h2>
+              <p>
+                {this.props.user.f_name
+                  ? `${this.props.user.f_name}`
+                  : "No first name on record."}
+              </p>
+              <input />
+              <button>SUBMIT</button>
+            </div>
+            <div className="user-info">
+              <h2>Update Last Name</h2>
+              <p>
+                {this.props.user.l_name
+                  ? `${this.props.user.l_name}`
+                  : "No last name on record."}
+              </p>
+              <input />
+              <button>SUBMIT</button>
+            </div>
+            <div className="user-info">
+              <h2>Update Email</h2>
+              <p>
+                {this.props.user.email
+                  ? `${this.props.user.email}`
+                  : "No email on record, input one below."}
+              </p>
+              <input />
+              <button>SUBMIT</button>
+            </div>
+          </div>
+          <div className="delete-user">
+            <h2>DELETE ACCOUNT</h2>
+            <p>
+              Deleting your user account means that we will no longer store your
+              information. However any links that you have created will still be
+              active and available to any with the link.
+            </p>
+            <button id="delete-user-btn">DELETE ACCOUNT</button>
+          </div>
         </div>
       </div>
     );
