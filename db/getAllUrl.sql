7cc40599c18d398c56bedc5374d9789307772780
Hayden Manning <haydenmanning@aol.com>
2018-03-16 22:19:35 -0500

    get users url query

--- db/getAllUrl.sql
+++ db/getAllUrl.sql
@@ -1,1 +1,4 @@
-SELECT * FROM url WHERE author = $1
+SELECT url.author, url.short_url, url.orig_url, users.auth_id, users.f_name, users.l_name
+FROM url
+INNER JOIN users ON url.author=users.uniq_user_id
+WHERE url.author = $1
